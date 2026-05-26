899b1695a8be5fcdba67bddf295173a1b5aeaf10
devdouglasbraga7 <devdouglasbraga7@gmail.com>
2026-05-25 22:18:42 -0300

    Adicionando menu de opções e pedindo pro cliente fazer a escolha

---
+++ main.py
@@ -0,0 +1,14 @@
+from modules.alunos import cadastrar_alunos
+
+while True:
+    print("=" * 50)
+    print("1 - Cadastrar aluno")
+    print("2 - Listar alunos")
+    print("3 - Alterar aluno")
+    print("4 - Remover aluno")
+    print("=" * 50)
+
+    escolha = int(input("Digite uma das opções numéricas: "))
+
+    if escolha == 1:
+        cadastrar_alunos()
