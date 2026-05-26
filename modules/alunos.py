41a1cc84f29d56a3da3010de1a15a5882a0a827a
devdouglasbraga7 <devdouglasbraga7@gmail.com>
2026-05-25 22:10:01 -0300

    adicionando função de cadastrar alunos no file modules

---
+++ modules/alunos.py
@@ -0,0 +1,17 @@
+alunos = []
+
+def cadastrar_alunos():
+    nome = input("Digite o nome: ")
+    idade = int(input("Digite a idade: "))
+    cpf = int(input("Digite o cpf: "))
+    cadastro = {"nome": nome,
+                "idade": idade,
+                "cpf": cpf,
+                }
+    alunos.append(cadastro)
+def listar_alunos():
+    pass
+def alterar_alunos():
+    pass
+def remover_aluno():
+    pass
