83c9b421efb914a8c6e7a4e2b2fdf059c4b53169
devdouglasbraga7 <devdouglasbraga7@gmail.com>
2026-05-29 16:00:42 -0300

    finalizando crud com funções

--- modules/alunos.py
+++ modules/alunos.py
@@ -1,5 +1,10 @@
 cadastro_aluno = []
+
+proximo_id = 1
 def cadastrar_alunos(cadastro_aluno):
+    
+    global proximo_id
+    
     nome = input("Digite o nome: ")
     idade = int(input("Digite a idade: "))
     cpf = int(input("Digite o cpf: "))
@@ -13,14 +18,30 @@ def cadastrar_alunos(cadastro_aluno):
     proximo_id += 1
     
 def listar_alunos(cadastro_aluno):
-    for i, nome in enumerate(cadastro_aluno, start=1):
-        print(i, "-", nome["nome"])
+    for aluno in cadastro_aluno:
+        print(aluno["id"], "-", aluno["nome"])
 def alterar_alunos(cadastro_aluno):
-    for i, nome in enumerate(cadastro_aluno, start=1):
-        print(i, "-" , nome)
-        opcao = int(input("Escolha a opção: "))
-        novo_nome = input("Digite o novo nome: ")
+    for aluno in cadastro_aluno:
+        print(aluno["id"], "-", aluno["nome"])
+        
+    opcao = int(input("Escolha o id do aluno: "))
+    
+    novo_nome = input("Digite o novo nome: ")
+
+    for aluno in cadastro_aluno:
+
+        if aluno["id"] == opcao:
+
+            aluno["nome"] = novo_nome
+
+            print("Nome alterado com sucesso!")        
+def remover_aluno(cadastro_aluno):
+    for aluno in cadastro_aluno:
+        print(aluno["id"], "-", aluno["nome"])
+    opcao = int(input("Digite o id do cadastro a ser excluído: "))
+    for aluno in cadastro_aluno:
+        if aluno["id"] == opcao:
+            cadastro_aluno.remove(aluno)
+            print("Cadastro excluído com sucesso!")
             
-        cadastro[opcao - 1]["nome"] = novo_nome
-def remover_aluno():
-    pass
+            break
