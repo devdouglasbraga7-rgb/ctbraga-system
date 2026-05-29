ebb69673adfa568b1954069194735d0d1484b556
devdouglasbraga7 <devdouglasbraga7@gmail.com>
2026-05-29 15:04:28 -0300

    Adicionando funções de listar e alterar e adicionando contador de id automático

--- modules/alunos.py
+++ modules/alunos.py
@@ -1,18 +1,26 @@
-alunos = []
-
-def cadastrar_alunos():
+cadastro_aluno = []
+def cadastrar_alunos(cadastro_aluno):
     nome = input("Digite o nome: ")
     idade = int(input("Digite a idade: "))
     cpf = int(input("Digite o cpf: "))
-    cadastro = {"nome": nome,
+    cadastro = {
+            "id": proximo_id,
+            "nome": nome,
                 "idade": idade,
                 "cpf": cpf,
                 }
-    alunos.append(cadastro)
-    alunos.append(cadastro)
-def listar_alunos():
-    pass
-def alterar_alunos():
-    pass
+    cadastro_aluno.append(cadastro)
+    proximo_id += 1
+    
+def listar_alunos(cadastro_aluno):
+    for i, nome in enumerate(cadastro_aluno, start=1):
+        print(i, "-", nome["nome"])
+def alterar_alunos(cadastro_aluno):
+    for i, nome in enumerate(cadastro_aluno, start=1):
+        print(i, "-" , nome)
+        opcao = int(input("Escolha a opção: "))
+        novo_nome = input("Digite o novo nome: ")
+            
+        cadastro[opcao - 1]["nome"] = novo_nome
 def remover_aluno():
     pass
